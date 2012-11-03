f5f04919def53699299da7556cdb0048ed183313
Sungho Hong <shhong@oist.jp>
2012-11-04 06:14:13 +0900

    IntroProbability.scala renamed to Probability.scala

--- Probability.scala
+++ Probability.scala
@@ -2,12 +2,11 @@ package rosalind
 /* (c) Copyright 2012 Sungho Hong. All Rights Reserved. */
 import common._
 
-object IntroProbability {
+object Probability {
+      def p(x: Double): Double = 2*(x*x/4) + 2*((1-x)*(1-x)/4)
+
   def main(args: Array[String]) {
       val a = read_data("prob")(args).replaceAll("\n","").split(" ").map(_.toDouble)
-      
-      def p(x: Double): Double = 2*(x*x/4) + 2*((1-x)*(1-x)/4)
-      
       println(a.map(x => "%.6f".format(p(x))).mkString(" "))
   }
 }

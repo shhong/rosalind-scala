17c21c88907c37b47191bc1e751af1e9a70064ed
Sungho Hong <shhong@oist.jp>
2012-11-04 01:59:18 +0900

    while0 is now type parametrized.

--- SpeedUpMotifFinding.scala
+++ SpeedUpMotifFinding.scala
@@ -3,14 +3,14 @@ package rosalind
 import common._
 
 object KMP {
-    def while0(cond: Int => Boolean)(f: Int => Int, j: Int): Int = 
+    def while0[T](cond: T => Boolean)(f: T => T, j: T): T = 
         if (cond(j)) while0(cond)(f, f(j)) else j
     
     def preprocess(p: String): List[Int] = {
         def loop(s:String, j: Int, b: List[Int]): List[Int] = s match {
             case "" => b.reverse
             case _ => {
-                val j1 = while0(x => x>=0 && s.head!=p(x))(x => b(b.size-x-1), j) + 1
+                val j1 = while0[Int](x => x>=0 && s.head!=p(x))(x => b(b.size-x-1), j) + 1
 //                println("%s %d".format(s.head, j) + " " + b.reverse.toString)
                 loop(s.tail, j1, j1::b)
             }
@@ -23,9 +23,9 @@ object KMP {
         def loop(i: Int, j: Int, report: List[Int]): List[Int] = 
             if (i==t.length) report.reverse
             else {
-                println("%d %d : %s".format(i, j, report.mkString(" ")))
-                val j1 = while0(x => x>=0 && t(i)!=p(x))(x => b(x), j) + 1
-                if (j1==p.length) loop(i+1, b(j1), (i-j+1)::report) 
+//                println("%d %d : %s".format(i, j, report.mkString(" ")))
+                val j1 = while0[Int](x => x>=0 && t(i)!=p(x))(x => b(x), j) + 1
+                if (j1==p.length) loop(i+1, b(j1), (i-j)::report) 
                 else loop(i+1, j1, report)
             }
         loop(0, 0, List())
@@ -38,6 +38,5 @@ object SpeedUpMotifFinding {
       val dnaString = read_data("kmp")(args).replaceAll("\n", "")
       
       println(KMP.preprocess(dnaString).drop(1).mkString(" "))
-      
    }
 }

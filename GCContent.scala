06b6d837ca83b43ef7f639db8025db88d808f5e9
Sungho Hong <shhong@oist.jp>
2012-11-04 06:14:54 +0900

    FASTA parsing code is now in common.readFASTA.

--- GCContent.scala
+++ GCContent.scala
@@ -3,16 +3,14 @@ package rosalind
 import common._
 
 object GCContent {
-  
+    
   def countGC(ncd: String): Double = ((ncd.count(_=='G') + ncd.count(_=='C'))*100.0)/(ncd.length)
   
   def main(args: Array[String]) {
       val lines = read_data("gc")(args)
       
-      val Process = "([a-zA-Z]+_[0-9]+)\n([ACTG\n]+)>".r
-      
-      (for (p <- Process.findAllIn(lines)) yield p match {
-        case Process(x, y) => (x, countGC(y.replaceAll("\n","")))
-      }).maxBy(_._2) match {case (x,y) => println("%s\n%.6f%%".format(x,y))}
+      readFASTA(lines)
+       .map(p => p match {case (x, y) => (x, countGC(y))})
+       .maxBy(_._2) match {case (x,y) => println("%s\n%.6f%%".format(x,y))}
    }
 }
--- common.scala
+++ common.scala
@@ -9,4 +9,11 @@ def read_data(casename: String)(args: Array[String]): String = {
     lines      
 }
 
+def readFASTA(s: String): Iterator[(String, String)] = {
+    val Process = ">([a-zA-Z]+_[0-9]+)\n([ACTG\n]+)".r
+    for (p <- Process.findAllIn(s)) yield p match {
+        case Process(x, y) => (x, y.replaceAll("\n", ""))
+    }
+}
+
 }
